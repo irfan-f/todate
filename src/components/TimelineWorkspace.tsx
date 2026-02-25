d1dcdd079ade1f3f16741e673602fd706c0bea1a
Irfan Filipovic <41347778+irfan-f@users.noreply.github.com>
2026-02-25 00:05:03 -0800

    Remove duplicate onRightPanelCollapsedChange prop

--- src/components/TimelineWorkspace.tsx
+++ src/components/TimelineWorkspace.tsx
@@ -21,7 +21,6 @@ const TimelineWorkspace = ({
   maxYear,
   defaultContent,
   onActiveChange,
-  onRightPanelCollapsedChange,
   onSpanChange,
   isRightPanelCollapsed = false,
   onRightPanelCollapsedChange,
@@ -36,7 +35,6 @@ const TimelineWorkspace = ({
   maxYear?: number;
   defaultContent?: React.ReactNode;
   onActiveChange?: (hasActive: boolean) => void;
-  onRightPanelCollapsedChange?: (collapsed: boolean) => void;
   onSpanChange?: (startYear: number, endYear: number) => void;
   isRightPanelCollapsed?: boolean;
   onRightPanelCollapsedChange?: (collapsed: boolean) => void;
